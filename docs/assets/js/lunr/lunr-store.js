1ce3ba9d6d1cb6ca1c4b511e7c5d5142f51633ac
Alex <alexstephens9@gmail.com>
2020-10-03 22:04:22 +0100

    Minor edits to DHW post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Generative Art: Greater than the Sum of its Parts",
         "excerpt":"What is generative art? Generative art refers to any art practice where the artist uses a system, such as a set of natural language rules, a computer program, a machine, or other procedural invention, which is set into motion with some degree of autonomy contributing to or resulting in a...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/generative/",
+        "url": "https://alex-stephens.github.io/blog/generative/",
         "teaser": null
       },{
         "title": "The Demon Haunted World of COVID-19",
-        "excerpt":"A couple of months ago, I came across a quote that has been circulating online a fair bit this year. The quote comes from the 1995 book The Demon Haunted World by the American astrophysicist and science communicator Carl Sagan. The quote is as follows: I have a foreboding of...","categories": [],
+        "excerpt":"A couple of months ago, I came across a quote that has been circulating online a fair bit this year. The quote comes from the 1995 book The Demon Haunted World by the American astrophysicist and science communicator Carl Sagan, and reads as follows: I have a foreboding of an...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/demon-haunted-world/",
+        "url": "https://alex-stephens.github.io/blog/demon-haunted-world/",
         "teaser": null
       }]
