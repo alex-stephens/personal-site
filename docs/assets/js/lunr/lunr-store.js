47fa2ea9cd1278525a70d3e681502682ae806faf
Alex <alexstephens9@gmail.com>
2020-10-18 16:57:38 +0100

    Production build

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "Generative Art: Greater than the Sum of its Parts",
         "excerpt":"What is generative art? Generative art refers to any art practice where the artist uses a system, such as a set of natural language rules, a computer program, a machine, or other procedural invention, which is set into motion with some degree of autonomy contributing to or resulting in a...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/generative/",
-        "teaser": null
+        "url": "https://alex-stephens.github.io/blog/generative/",
+        "teaser": "https://alex-stephens.github.io/assets/images/teaser.png"
       },{
         "title": "The Demon Haunted World of COVID-19",
         "excerpt":"A couple of months ago, I came across a quote that has been circulating online a fair bit this year. The quote comes from the 1995 book The Demon Haunted World by the American astrophysicist and science communicator Carl Sagan, and reads as follows: I have a foreboding of an...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/demon-haunted-world/",
-        "teaser": null
+        "url": "https://alex-stephens.github.io/blog/demon-haunted-world/",
+        "teaser": "https://alex-stephens.github.io/assets/images/teaser.png"
       },{
         "title": "The Rule of Six",
         "excerpt":"About a month ago, I moved from Australia to the UK to start a PhD at the University of Oxford. While I was quite excited for the move despite the uncertainty of relocating halfway around the world during a global pandemic, I still arrived with the expectation that I would...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/rule-of-six/",
-        "teaser": null
+        "url": "https://alex-stephens.github.io/blog/rule-of-six/",
+        "teaser": "https://alex-stephens.github.io/assets/images/teaser.png"
       }]
