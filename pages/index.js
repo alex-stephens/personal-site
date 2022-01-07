7d76a7d464a363a59fae2c479c963cb70f86015e
Alex Stephens <alexstephens9@gmail.com>
2022-01-07 12:22:30 +1100

    Fix indentation of dates at small widths

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home({ posts }) {
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                     <dl>
                       <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium indent-4 leading-6 text-gray-500 dark:text-gray-400">
+                      <dd className="text-base font-medium xl:indent-4 leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
